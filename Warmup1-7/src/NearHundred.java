565c40a0ff6593d7762d62546cfbe6e282f77736
Alex <t89137899903@gmail.com>
2018-08-08 21:19:45 +0300

    solved Warmup1-7 updated gitignore: deleted *.iml files

--- Warmup1-7/src/NearHundred.java
+++ Warmup1-7/src/NearHundred.java
@@ -1,10 +1,11 @@
-/**http://codingbat.com/prob/p184004
- Given an int n, return true if it is within 10 of 100 or 200.
- Note: Math.abs(num) computes the absolute value of a number.
-
- nearHundred(93) → true
- nearHundred(90) → true
- nearHundred(89) → false
+/**
+ * http://codingbat.com/prob/p184004
+ * Given an int n, return true if it is within 10 of 100 or 200.
+ * Note: Math.abs(num) computes the absolute value of a number.
+ * <p>
+ * nearHundred(93) → true
+ * nearHundred(90) → true
+ * nearHundred(89) → false
  */
 //public boolean nearHundred(int n) {
 //  {
@@ -12,8 +13,8 @@
 
 /*** SOLUTION ***/
 public class NearHundred {
-    public boolean nearHundred(int n){
-
+    public boolean nearHundred(int n) {
+        return (Math.abs(100 - n) <= 10 || Math.abs(200 - n) <= 10);
     }
 
     public static void main(String[] args) {
