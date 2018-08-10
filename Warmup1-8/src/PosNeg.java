b3135a7b8796ee4d2e7e285ad90e748fbfb77d7b
AlexPreved <10qpalzm10>
2018-08-10 10:53:28 +0300

    solved 1-10

--- Warmup1-8/src/PosNeg.java
+++ Warmup1-8/src/PosNeg.java
@@ -1,10 +1,11 @@
-/**http://codingbat.com/prob/p159227
- Given 2 int values, return true if one is negative and one is positive.
- Except if the parameter "negative" is true, then return true only if both are negative.
-
- posNeg(1, -1, false) → true
- posNeg(-1, 1, false) → true
- posNeg(-4, -5, true) → true
+/**
+ * http://codingbat.com/prob/p159227
+ * Given 2 int values, return true if one is negative and one is positive.
+ * Except if the parameter "negative" is true, then return true only if both are negative.
+ * <p>
+ * posNeg(1, -1, false) → true
+ * posNeg(-1, 1, false) → true
+ * posNeg(-4, -5, true) → true
  */
 //  public boolean posNeg(int a, int b, boolean negative) {
 //  {
@@ -12,7 +13,33 @@
 
 /*** SOLUTION ***/
 public class PosNeg {
-    public boolean posNeg(int a, int b, boolean negative){
+    public boolean posNeg(int a, int b, boolean negative) {
+//        if (!negative && (a < 0 && b > 0 || a > 0 && b < 0)) return true;
+//        else if (negative && (a < 0 && b < 0)) return true;
+
+//        else return false;
+
+        return (!negative && (a < 0 && b > 0 || a > 0 && b < 0) || negative && (a < 0 && b < 0));
+//        if (negative) {
+//            if (a < 0 && b < 0) return true;
+//            else return false;
+//        } else {
+//            if ((a < 0 && b > 0) || (a > 0 && b < 0)) return true;
+//            else return false;
+//        }
+
+//        public boolean posNeg(int a, int b, boolean negative) {
+//            if (negative) {
+//                return (a < 0 && b < 0);
+//            }
+//            else {
+//                return ((a < 0 && b > 0) || (a > 0 && b < 0));
+//            }
+//        }
+
+    }
+
+    public static void main(String[] args) {
 
     }
 }
