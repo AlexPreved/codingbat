849c6c67e551c136b567663bea2724cd93e1bddd
AlexPreved <10qpalzm10>
2018-08-10 16:47:16 +0300

    solved 1-11

---
+++ Warmup1-12/src/FrontBack.java
@@ -0,0 +1,18 @@
+/**http://codingbat.com/prob/p123384
+ Given a string, return a new string where the first and last chars have been exchanged.
+
+ frontBack("code") → "eodc"
+ frontBack("a") → "a"
+ frontBack("ab") → "ba"
+ */
+
+
+public class FrontBack {
+    public String frontBack(String str) {
+//        String first = str.substring(0,1);
+//        String mid = str.substring(1,str.length()-1);
+//        String last = str.substring(str.length()-1, str.length());
+//
+//        return  last + mid + first;
+    }
+}
