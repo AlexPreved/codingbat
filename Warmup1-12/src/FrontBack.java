619f4842922cef0c2551ad33ff639a91f7cdd7bb
AlexPreved <10qpalzm10>
2018-08-15 09:23:39 +0300

    solved 1-12 with charAt + substring

--- Warmup1-12/src/FrontBack.java
+++ Warmup1-12/src/FrontBack.java
@@ -9,15 +9,23 @@
 
 public class FrontBack {
     public String frontBack(String str) {
-        if (str.length()>1) {
-
-        String first = str.substring(0,1);
-        String mid = str.substring(1,str.length()-1);
-        String last = str.substring(str.length()-1, str.length());
-
-        return  last + mid + first;}
-
+        if (str.length() > 1) {
+            char first = str.charAt(0);
+            char last = str.charAt(str.length() -1);
+            String mid = str.substring(1, str.length() - 1);
+            return last + mid + first;
+        }
         return str;
+
+//        if (str.length()>1) {
+//
+//        String first = str.substring(0,1);
+//        String mid = str.substring(1,str.length()-1);
+//        String last = str.substring(str.length()-1, str.length());
+//
+//        return  last + mid + first;}
+//
+//        return str;
     }
 
     public static void main(String[] args) {
