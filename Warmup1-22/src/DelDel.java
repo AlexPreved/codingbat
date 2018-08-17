673da13013d8a33d697d810ff05c45365c9f56ba
AlexPreved <10qpalzm10>
2018-08-17 08:51:54 +0300

    solved 1-22

--- Warmup1-22/src/DelDel.java
+++ Warmup1-22/src/DelDel.java
@@ -10,7 +10,7 @@
 
 public class DelDel {
     public String delDel (String str) {
-        if (str.substring(1, 4).equals("del")) {
+        if (str.length () >= 4 && str.substring(1, 4).equals("del")) {
             return str.charAt(0) + str.substring(4, str.length());
         }
         return str;
