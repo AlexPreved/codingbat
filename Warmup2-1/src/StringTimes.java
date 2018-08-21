6165b66e92802fa3a31cd9c7efc85d6c3c571669
Alex <t89137899903@gmail.com>
2018-08-21 20:44:29 +0300

    solved 2-1: Flowless victory!

---
+++ Warmup2-1/src/StringTimes.java
@@ -0,0 +1,20 @@
+/**https://codingbat.com/prob/p142270
+ Given a string and a non-negative int n, return a larger string that is n copies of the original string.
+
+ stringTimes("Hi", 2) → "HiHi"
+ stringTimes("Hi", 3) → "HiHiHi"
+ stringTimes("Hi", 1) → "Hi"
+ */
+
+
+public class StringTimes {
+    public String stringTimes(String str, int n) {
+        String ret = "";
+        for (int i =0; i < n; i++){
+            ret += str;
+        }
+        return ret;
+
+
+    }
+}
