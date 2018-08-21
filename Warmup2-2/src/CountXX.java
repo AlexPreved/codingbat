552f5425739ed8aae6702ca2a84f03949911900a
Alex <t89137899903@gmail.com>
2018-08-21 20:51:04 +0300

    solved 2-2: вычисление количества хх в строке с перехлестом через CharAt

---
+++ Warmup2-2/src/CountXX.java
@@ -0,0 +1,19 @@
+/**https://codingbat.com/prob/p194667
+ Count the number of "xx" in the given string. We'll say that overlapping is allowed, so "xxx" contains 2 "xx".
+
+ countXX("abcxx") → 1
+ countXX("xxx") → 2
+ countXX("xxxx") → 3
+ */
+
+public class CountXX {
+    public int countXX(String str, int n){
+        /** решить через substring */
+
+//        int counter = 0;
+//        for (int i = 1; i < str.length(); i++){
+//            if(str.charAt(i) == 'x' && str.charAt(i-1) == 'x') counter += 1;
+//        }
+//        return counter;
+    }
+}
